c63a027e36658aac2d75aabf90f7669e3ff11931
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-02-10 09:42:53 -0700

    Context and trees object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { createContext, useContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const TreesContext = createContext();
 
+export const useTrees = () => useContext(TreesContext);
+
+const trees = [
+    {id: '1', type: 'Maple'},
+    {id: '2', type: 'Sapele'},
+    {id: '3', type: 'Wawa'},
+    {id: '4', type: 'Odum'},
+];
 
 ReactDOM.render(
-  <React.StrictMode>
+  <TreesContext.Provider value={{ trees }}>
     <App />
-  </React.StrictMode>,
+  </TreesContext.Provider>,
   document.getElementById('root')
 );
