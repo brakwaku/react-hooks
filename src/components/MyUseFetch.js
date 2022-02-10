50d1e320d125151f47463c805846070fcebf938a
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2022-02-10 09:43:35 -0700

    Custom hook to fetch data from api

---
+++ src/components/MyUseFetch.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+// States to consider:
+// If the data is not available yet but is loading
+// If the data is available
+// If an error occurs
+export function useFetch(uri) {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    if (!uri) return;
+
+    fetch(uri)
+      .then((data) => data.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [uri]);
+
+  return { loading, data, error };
+}
